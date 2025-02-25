e2096d95d24b7fc8b6366d383035488f29b2091d
AnaBear27 <anabeatriz6878@gmail.com>
2025-02-25 09:05:48 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,8 @@
+#ARMAZENANDO E EXIBINDO INFORMAÇÕES.
+
+nome = "João"
+idade = 30
+altura = 1.75
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura, "metros.")
---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+#CALCÚLOS COM VARIÁVEIS.
+
+numero1 = 10
+numero2 = 5
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+
+print("1º número:", numero1)
+print("2º número:", numero2)
+print("Soma:", soma)
+print("Subtração:", subtracao)
